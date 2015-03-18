5d0673695b1a85a9f012de62475953096c4929d5
Zygimantas <zygimantas.simkus@gimo.co.uk>
2015-03-18 11:29:00 +0000

    test

--- src/AppBundle/Controller/MasterController.php
+++ src/AppBundle/Controller/MasterController.php
@@ -16,4 +16,63 @@ class ServiceController
         $this->em = $em;
     }
 
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        if(!$this->isLoggedIn())
+        {
+            // return $this->redirect($this->generateUrl('login'));
+        }
+
+        $admin = $this->getAdminDetails(2);
+        $sites = $this->getManagedSites();
+
+        return $this->render('master/index.html.twig', array('administrator' => $admin, 'sites' => $sites));
+    }
+
+
+    private function getAdminDetails($id)
+    {
+        $em                = $this->getDoctrine()->getManager();
+        $repository        = $em->getRepository('AppBundle:Admin');
+        $administrator     = $repository->findOneById($id);
+
+        $admin['name']     = $administrator->getName();
+        $admin['id']       = $administrator->getId();
+        $admin['password'] = $administrator->getPassword();
+        $admin['picture']  = $administrator->getPicture();
+
+        $administrator->setLastLoggedIn(new \DateTime(date('Y-m-d H:i:s')));
+        $administrator->setCurrentLocation($this->request->getPathInfo());
+        $em->flush();
+
+        return is_array($admin) && !is_null($admin) ? $admin : false;
+    }
+
+    private function getManagedSites()
+    {
+        $em         = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository('AppBundle:Sites');
+        $sites      = $repository->findAll();
+
+        return $sites;
+    }
+
+    private function isLoggedIn()
+    {
+        return $this->container->get('session')->isStarted();
+    }
+
+
+    /**
+     * @Route("/error/", name="error")
+     */
+    public function notLoggedIn()
+    {
+        return $this->render('login/index.html.twig');
+    }
+
+
 }
