5f75f1d6341033e4dcfca73df4c7eec5fd404b6a
Zygimantas <zygimantas.simkus@gimo.co.uk>
2015-03-18 11:33:43 +0000

    test

--- src/AppBundle/Controller/MasterController.php
+++ src/AppBundle/Controller/MasterController.php
@@ -31,6 +31,7 @@ class MasterController extends Controller
      */
     public function indexAction()
     {
+        $entityManager = $em->getDoctrine()->getManager();
         if(!$this->isLoggedIn())
         {
             // return $this->redirect($this->generateUrl('login'));
