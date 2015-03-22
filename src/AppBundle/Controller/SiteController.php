f4c991e11f44bea170bf392360cb17062bf05e83
Zygimantas Simkus <z.simkus@yahoo.com>
2015-03-22 17:19:24 +0000

    test

--- src/AppBundle/Controller/SiteController.php
+++ src/AppBundle/Controller/SiteController.php
@@ -139,7 +139,7 @@ class SiteController extends Controller
         else
         {
             $errorTitle       = 'Error connecting to SQL';
-            $errorDescription = 'Incorrect credentials. error at ' . __LINE__ . ' in ' . __FILE__;
+            $errorDescription = 'Incorrect credentials. Error at ' . __LINE__ . ' in ' . __FILE__;
             $errors = new Errors();
             $errors->logError($errorTitle, $errorDescription);
             $em = $this->getDoctrine()->getManager();
