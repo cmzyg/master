87b9d5e788ab8e9a1b7ad2f29dee413dd8779d55
Zygimantas Simkus <z.simkus@yahoo.com>
2015-03-22 17:52:02 +0000

    test

---
+++ src/AppBundle/Controller/ErrorsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\Entity;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Entity\Errors;
+
+
+
+class ErrorsController extends Controller
+{
+    private $request;
+    private $session;
+    private $filesystem;
+    private $finder;
+
+
+    public function __construct()
+    {
+        //...
+    }
+
+    private function checkConnection($errorTitle, $errorDescription)
+    {
+        $errorTime        = date('d-m-Y H:i:s');
+        $errors = new Errors();
+        $errors->logError($errorTitle, $errorDescription, $errorTime);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($errors);
+        $em->flush();
+    }
+
+
+
+}
--- src/AppBundle/Controller/SiteController.php
+++ src/AppBundle/Controller/SiteController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
 use AppBundle\Entity\Errors;
+use AppBundle\Controller\ErrorsController;
 
 
 
