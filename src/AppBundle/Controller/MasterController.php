3f91fe90cff0eb1c59294c7aae6d9f9a208d752b
Zygimantas Simkus <z.simkus@yahoo.com>
2015-03-22 17:24:17 +0000

    test

--- src/AppBundle/Controller/MasterController.php
+++ src/AppBundle/Controller/MasterController.php
@@ -34,7 +34,7 @@ class MasterController extends Controller
 
         $admin  = $this->getAdminDetails(2);
         $sites  = $this->getManagedSites();
-        $errors = $this->getErrors(5);
+        $errors = $this->getErrors(3);
 
         return $this->render('master/index.html.twig', array('administrator' => $admin, 'sites' => $sites, 'errors' => $errors));
     }
