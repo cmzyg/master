c209cb9183f24671517e69047cd9fac010b12006
Zygimantas Simkus <z.simkus@yahoo.com>
2015-03-22 20:00:48 +0000

    test

--- src/AppBundle/Controller/MasterController.php
+++ src/AppBundle/Controller/MasterController.php
@@ -21,7 +21,6 @@ class MasterController extends Controller
     {
         $this->request = Request::createFromGlobals();
         $this->session = new Session;
-        $this->logger  = $this->get('app.errors_controller');
     }
 
     /**
@@ -38,6 +37,8 @@ class MasterController extends Controller
         $sites  = $this->getManagedSites();
         $errors = $this->getErrors(3);
         $helper = $this->get('app.errors_controller');
+        
+        $this->logger  = $this->get('app.errors_controller');
         $this->logger->log('test', 'tetsing - works!');
 
         return $this->render('master/index.html.twig', array('administrator' => $admin, 'sites' => $sites, 'errors' => $errors));
