136396635d84294284c88c1d0ac92365968b0c7a
Zygimantas Simkus <z.simkus@yahoo.com>
2015-03-25 12:03:31 +0000

    test

--- src/AppBundle/Controller/ErrorLogController.php
+++ src/AppBundle/Controller/ErrorLogController.php
@@ -27,8 +27,9 @@ class ErrorLogController extends Controller
      */
     public function indexAction()
     {
+        $admin  = $this->getAdminDetails(2);
         $errors = $this->getErrors(30);
-        return $this->render('error-log/index.html.twig', array('errors' => $errors));
+        return $this->render('error-log/index.html.twig', array('errors' => $errors, 'administrator' => $admin));
     }
 
     private function getErrors($limit)
@@ -38,6 +39,24 @@ class ErrorLogController extends Controller
         $errors     = $repository->findBy(array(), array('id' => 'DESC'), $limit);
 
         return $errors;
+    }
+
+    private function getAdminDetails($id)
+    {
+        $em                = $this->getDoctrine()->getManager();
+        $repository        = $em->getRepository('AppBundle:Admin');
+        $administrator     = $repository->findOneById($id);
+
+        $admin['name']     = $administrator->getName();
+        $admin['id']       = $administrator->getId();
+        $admin['password'] = $administrator->getPassword();
+        $admin['picture']  = $administrator->getPicture();
+
+        $administrator->setLastLoggedIn(new \DateTime(date('Y-m-d H:i:s')));
+        $administrator->setCurrentLocation($this->request->getPathInfo());
+        $em->flush();
+
+        return is_array($admin) && !is_null($admin) ? $admin : false;
     }
 
 
