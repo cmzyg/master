48e02eb1052d1724658a155a2690b55f2214d0c8
Zygimantas Simkus <z.simkus@yahoo.com>
2015-04-06 12:39:58 +0100

    test

--- src/AppBundle/Controller/AuthController.php
+++ src/AppBundle/Controller/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
      */
     public function indexAction()
     {
-        if($this->isLoggedIn() == NULL)
+        if($this->isLoggedIn() ! == NULL)
         {
             return $this->redirect($this->generateUrl('homepage'));
         }
