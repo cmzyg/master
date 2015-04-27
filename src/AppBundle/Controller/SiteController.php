a882e496fe2e837852735cda87d947b15224353d
Zygimantas Simkus <z.simkus@yahoo.com>
2015-04-27 22:09:32 +0100

    test

--- src/AppBundle/Controller/SiteController.php
+++ src/AppBundle/Controller/SiteController.php
@@ -37,7 +37,7 @@ class SiteController extends Controller
      */
     public function indexAction($id)
     {
-        $siteService = $this->get('app.includes_controller');
+        $siteService = $this->get('app.site_service_controller');
 
 
         $admin  = $this->getAdminDetails(2);
@@ -66,6 +66,11 @@ class SiteController extends Controller
                 foreach ($this->finder as $file) 
                 {
                     $fileList[] = $file->getRealpath();
+                }
+
+                if($siteService->establishDatabaseConnection($siteConfig['dbhost'], $siteConfig['dbuser'], $siteConfig['dbpass'], $siteConfig['dbname']))
+                {
+                    die('zygis');
                 }
 
                 return $this->render('AppBundle:site:index.html.twig', array('pageId' => 2, 'administrator' => $admin, 'siteInfo' => $siteInfo, 'siteConfig' => $siteConfig, 'connectionStatus' => $connectionStatus, 'id' => $id, 'fileList' => $fileList, 'fileCount' => sizeof($fileList)));
